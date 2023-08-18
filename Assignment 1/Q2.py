6a380727a47eac401d2ff51ded5f6b9faa8e8862
haniyeh4 <116498371+haniyeh4@users.noreply.github.com>
2023-08-17 22:37:14 -0400

    Update Q2.py

--- Assignment 1/Q2.py
+++ Assignment 1/Q2.py
@@ -2,52 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
-# Set the parameters
-r = 1
-theta_deg = 0.5
-mean_x = r * np.cos(np.deg2rad(theta_deg))
-mean_y = r * np.sin(np.deg2rad(theta_deg))
-cov_x = np.array([[0.01, 0], [0, 0.005]])
-cov_y = np.zeros((2, 2))
+# Define parameters
+np.random.seed(42)
+mean = np.array([1, np.radians(0.5)])  # Convert degrees to radians
+cov = np.array([[0.01, 0], [0, 0.005]])
 
-# Generate samples from the distribution
-n_samples = 1000
-samples = np.random.multivariate_normal([mean_x, mean_y], cov_x, size=n_samples)
+# Monte Carlo
+num_samples = 1000
+samples = np.random.multivariate_normal(mean, cov, num_samples)
 
-# Transform the samples
-transformed_samples = np.column_stack([
-    samples[:, 0] * np.cos(samples[:, 1] - r * samples[:, 1] * np.sin(samples[:, 1])),
-    samples[:, 0] * np.sin(samples[:, 1]) + r * samples[:, 1] * np.cos(samples[:, 1])
-])
+# Create the scatter plot
+plt.scatter(samples[:, 0], samples[:, 1], s=10, label='Simulated Points')
 
-# Calculate the covariance matrix for transformed samples
-theta_rad = np.deg2rad(theta_deg)
-cov_y[0, 0] = 9.9996 * (10 ** -3)
-cov_y[0, 1] = 4.3631 * (10 ** -3)
-cov_y[1, 0] = cov_y[0, 1]
-cov_y[1, 1] = 5.0004 * (10 ** -3)
+s=np.sqrt(5.991) 
+eigvals, eigvecs = np.linalg.eig(cov)
+angle = np.degrees(np.arctan2(eigvecs[1, 0], eigvecs[0, 0]))
+ellipse = Ellipse(mean, 2 * s * np.sqrt(eigvals[0]), 2 * s * np.sqrt(eigvals[1]),
+                  angle=angle, edgecolor='r', facecolor='none', label='Uncertainty Ellipse')
+plt.gca().add_patch(ellipse)
 
-# Calculate eigenvalues and eigenvectors of the covariance matrix
-eigenvalues, eigenvectors = np.linalg.eig(cov_y)
-angle_rad = np.arctan2(eigenvectors[0, 1], eigenvectors[0, 0])
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.grid()
 
-# Plot the transformed points and ellipse
-fig, ax = plt.subplots()
-ax.scatter(transformed_samples[:, 0], transformed_samples[:, 1], marker='.', color='blue')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_title('Transformed Points and Ellipse')
-ax.grid(True)
-
-# Set the confidence level and ellipse parameters
-confidence_level = 0.95
-chi_square_value = np.sqrt(5.991)
-width = 2 * np.sqrt(eigenvalues[0]) * chi_square_value
-height = 2 * np.sqrt(eigenvalues[1]) * chi_square_value
-
-# Add the ellipse to the plot
-ellipse = Ellipse((mean_x, mean_y), width, height, color='blue', angle=np.rad2deg(angle_rad), alpha=0.2)
-ax.add_patch(ellipse)
-
-# Show the plot
+# Show or save the plot
 plt.show()
